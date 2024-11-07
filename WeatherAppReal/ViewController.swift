ae35a3b287e1bcc589c18ea505c4aa7771a2d4b1
Khalid Olowe-Makorie <khalid.olowemakorie@vodafone.com>
2024-11-07 15:31:17 +0000

    Initial Commit

---
+++ WeatherAppReal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppReal
+//
+//  Created by "Khalid Olowe-Makorie, Vodafone" on 07/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherAppRealTests/WeatherAppRealTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppRealTests.swift
+//  WeatherAppRealTests
+//
+//  Created by "Khalid Olowe-Makorie, Vodafone" on 07/11/2024.
+//
+
+import Testing
+@testable import WeatherAppReal
+
+struct WeatherAppRealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
